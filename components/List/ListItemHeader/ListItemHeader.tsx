3f7a0471575bb2d10b98da5eda1c8131bf111458
cohenaj194 <cohenaj194@gmail.com>
2024-07-06 15:57:06 -0400

    add links to list item headers, closes: https://github.com/Universalis-FFXIV/mogboard-next/issues/155

--- components/List/ListItemHeader/ListItemHeader.tsx
+++ components/List/ListItemHeader/ListItemHeader.tsx
@@ -21,7 +21,6 @@ export default function ListItemHeader({ item, lang }: ListItemHeaderProps) {
   return (
     <>
       {item.levelItem > 1 && <em className="ilv">{item.levelItem}</em>}
-      // @coderabbitai
       <Link href="/market/[itemId]" as={`/market/${item.id}`}>
         <a className={`rarity-${item.rarity}`}>{item.name}</a>
       </Link>
@@ -30,6 +29,29 @@ export default function ListItemHeader({ item, lang }: ListItemHeaderProps) {
       <span>
         {itemKind?.name} - {itemSearchCategory?.name}
       </span>
+      <div className="external-links">
+        <a
+          href={`https://saddlebagexchange.com/queries/item-data/${item.id}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button type="button" className="btn btn_gt">Saddlebag Exchange</button>
+        </a>
+        <a
+          href={`https://www.garlandtools.org/db/#item/${item.id}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button type="button" className="btn btn_gt">GarlandTools</button>
+        </a>
+        <a
+          href={`https://ffxivteamcraft.com/db/en/item/${item.id}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button type="button" className="btn btn_gt">Teamcraft</button>
+        </a>
+      </div>
     </>
   );
 }
